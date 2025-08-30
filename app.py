00cf0fa77796f2ab043595b353bb25270782e15f
AmedhPujar <102457315+AmedhPujar@users.noreply.github.com>
2025-08-30 11:39:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from database import init_db
 from recruiter_ui import recruiter_tab
 from student_ui import student_tab
 
-st.set_page_config(page_title="Career Connector App (AI-powered)", layout="wide")
-st.title("Career Connector App")
+st.set_page_config(page_title="Enter your title", layout="wide")
+st.title("Enter your title")
 
 tabs = st.tabs(["Recruiter", "Student"])
 
@@ -16,3 +16,4 @@ with tabs[1]:
 
 if __name__ == "__main__":
     init_db()
+
