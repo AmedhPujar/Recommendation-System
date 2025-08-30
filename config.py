b1f7df0bbd0ad1aa1979b74a1eaa2c11fe562279
AmedhPujar <102457315+AmedhPujar@users.noreply.github.com>
2025-08-30 11:40:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-DB_PATH = "career_connector.db"
+DB_PATH = "Your Database Path"
 JWT_SECRET = os.getenv("JWT_SECRET")
-JWT_ALGORITHM = "HS256"
+JWT_ALGORITHM = "Your Algorithm"
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-JOBDATA_PATH = os.path.join(os.path.dirname(__file__), "Career_connector_app", "JobData.csv")
+
+JOBDATA_PATH = os.path.join(os.path.dirname(__file__), "Your Folder Name", "Your Training Data")
